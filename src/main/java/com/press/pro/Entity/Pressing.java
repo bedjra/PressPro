432d43564038dd89d47fc9946e9af74892d48b9d
bedjra <armelbedjra12@gmail.com>
2025-11-05 11:07:18 +0000

    Description

--- src/main/java/com/press/pro/Entity/Pressing.java
+++ src/main/java/com/press/pro/Entity/Pressing.java
@@ -1,9 +1,7 @@
 package com.press.pro.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
+
 
 @Entity
 public class Pressing {
